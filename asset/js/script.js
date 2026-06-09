0ac4776286ca890415b1dd0ee4291ec64f58b4e2
Jasdegst <alhusaini124887@gmail.com>
2026-06-10 03:18:55 +0700

    ganti sekali lagi secript java secript

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,24 +1,60 @@
-try {
+const form = document.getElementById("orderForm");
+const statusText = document.getElementById("status");
 
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbwdK5bh0oZmNk0S_ZWGoDbGnCSHntHTKJax9813Z3qQh4jSJXf0unmLDPqg6jON_q7OOA/exec",
-    {
-      method: "POST",
-      body: JSON.stringify(data)
+form.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  statusText.innerHTML = "Mengirim pesanan...";
+
+  const data = {
+    nama: document.getElementById("nama").value,
+    hp: document.getElementById("hp").value,
+    produk: document.getElementById("produk").value,
+    jumlah: document.getElementById("jumlah").value,
+    alamat: document.getElementById("alamat").value
+  };
+
+  try {
+
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbyyiFLxQ3ap29RuinFjMwlJF6X5V3-YgL60rlCNpE5TBhfc3KSYZP-D1ouDQ0Rw25uHwA/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    );
+
+    const result = await response.text();
+
+    console.log("Response:", result);
+
+    if (response.ok) {
+
+      statusText.innerHTML =
+        "✅ Pesanan berhasil dikirim!";
+
+      form.reset();
+
+    } else {
+
+      statusText.innerHTML =
+        "❌ Server mengembalikan error.";
+
+      console.error(result);
+
     }
-  );
 
-  const result = await response.text();
+  } catch (error) {
 
-  console.log(result);
+    console.error("ERROR:", error);
 
-  document.getElementById("status")
-    .innerHTML = "Pesanan berhasil dikirim!";
+    statusText.innerHTML =
+      "❌ Gagal mengirim pesanan.";
 
-} catch(error) {
+  }
 
-  console.error(error);
-
-  document.getElementById("status")
-    .innerHTML = error.message;
-}
+});
